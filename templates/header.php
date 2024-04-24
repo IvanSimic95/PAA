5c673fa62bb92e04319b6716f18b9ba7005d3cb8
IvanSimic95 <email@isimic.com>
2024-04-24 17:11:15 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -55,7 +55,17 @@
     <link href="/assets/css/review.css" rel="stylesheet">
     <link href="/assets/css/custom.css?v=1204" rel="stylesheet">
    
-
+<!-- Hotjar Tracking Code for https://soulmate-drawing.com/ -->
+  <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:4957787,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 
     <!--CUSTOMCSS-->
 
